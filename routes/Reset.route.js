1a63c76f276f132441c11025f10b0d517550cde1
Hedayat Farahi <Hedayatullah.farahi@oerlikon.com>
2023-04-11 16:47:14 +0000

    add reset route

--- routes/Reset.route.js
+++ routes/Reset.route.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const createError = require("http-errors");
 const User = require("../models/User.model");
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
 
 router.post("/:id", async (req, res, next) => {
 
@@ -13,20 +12,12 @@ router.post("/:id", async (req, res, next) => {
         const user = await User.find({ "_id": req.params.id });
 
         if (!user) {
-          next(createError(401, "User not found"));
-          return;
+            next(createError(401, "User not found"));
+            return;
         }
 
-        const isMatch = await bcrypt.compare(currentPassword, user.password);
+        res.send(user);
 
-        if (!isMatch) {
-          next(createError(401, "Password is incorrect"));
-          return;
-        }
-
-        const token = jwt.sign({ id: user._id, email: user.username, storeName: user.storeName, mainColor: user.mainColor, currency: user.currency }, process.env.JWT_SECRET);
-
-        res.status(200).json({ token });
     } catch (err) {
         if (err.name === "ValidationEroor") {
             next(createError(422, err.message));
