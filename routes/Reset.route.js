74b6b847bf9c05758c92254fa586c164ec21c03c
Hedayat Farahi <Hedayatullah.farahi@oerlikon.com>
2023-04-11 16:45:14 +0000

    add reset route

--- routes/Reset.route.js
+++ routes/Reset.route.js
@@ -13,51 +13,20 @@ router.post("/:id", async (req, res, next) => {
         const user = await User.find({ "_id": req.params.id });
 
         if (!user) {
-            next(createError(401, "User not found"));
-            return;
+          next(createError(401, "User not found"));
+          return;
         }
 
-        isMatch = false;
-
-        bcrypt.compare(currentPassword, user.password, function (err, result) {
-            isMatch = true;
-        });
+        const isMatch = await bcrypt.compare(currentPassword, user.password);
 
         if (!isMatch) {
-            next(createError(401, "Password is incorrect"));
-            return;
+          next(createError(401, "Password is incorrect"));
+          return;
         }
 
-        const result = await User.findByIdAndUpdate(req.params.id, { password: newPassword });
+        const token = jwt.sign({ id: user._id, email: user.username, storeName: user.storeName, mainColor: user.mainColor, currency: user.currency }, process.env.JWT_SECRET);
 
-        res.send(result);
-
-
-
-        // const user = await User.findByIdAndUpdate(id, updates, options);
-
-        // if (!user) {
-        //   throw createError(404, "User does not exist");
-        // }
-        // res.send(user);
-
-        // const user = await User.findOne({ username });
-
-        // if (!user) {
-        //   next(createError(401, "User not found"));
-        //   return;
-        // }
-
-        // const isMatch = await bcrypt.compare(password, user.password);
-
-        // if (!isMatch) {
-        //   next(createError(401, "Password is incorrect"));
-        //   return;
-        // }
-
-        // const token = jwt.sign({ id: user._id, email: user.username, storeName: user.storeName, mainColor: user.mainColor, currency: user.currency }, process.env.JWT_SECRET);
-
-        // res.status(200).json({ token });
+        res.status(200).json({ token });
     } catch (err) {
         if (err.name === "ValidationEroor") {
             next(createError(422, err.message));
